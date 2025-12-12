9439a00467f3d22e25d680f8d859a306ef6fc619
JacksonBennett05 <jcb263@gmail.com>
2025-12-12 09:09:36 -0500

    Connect API + player form

--- nbaProj.js
+++ nbaProj.js
@@ -13,6 +13,8 @@ app.set("views", __dirname);
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(__dirname));
+
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
@@ -31,10 +33,53 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/playerForm", (req,res) => {
+  res.render("playerForm")
+})
+
+app.post("/getRoster", (req, res) => {
+  const names = [
+    req.body.p1,
+    req.body.p2,
+    req.body.p3,
+    req.body.p4,
+    req.body.p5
+  ];
+  res.render("playerResults", { players: results })
+})
+
 // Show page where user builds lineup
 
 
 //Save Lineup
+
+app.get("/apitest", async (req, res) => {
+  const fullName = "LeBron James";
+  const [first, last] = fullName.trim().split(" ");
+
+  const url = `https://v2.nba.api-sports.io/players?name=${last}`;
+
+  try {
+    const response = await fetch(url, {
+        headers: {
+          // In .env name api key "RAPID_API_KEY"
+          // Disclaimer only 100 requests per day
+          "x-apisports-key": process.env.RAPID_API_KEY
+        }
+      });
+
+    const data = await response.json();
+    const player = data.response.find(
+      p => p.firstname.toLowerCase() === first.toLowerCase()
+    );
+    res.send(player || "Player not found");
+  } catch (err) {
+    console.error(err);
+    res.send("API test failed");
+  }
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
